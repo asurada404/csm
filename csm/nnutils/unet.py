7a4307123a78a01f199967805f703e7948c4ec66
liuyi <yiliuecust@163.com>
2020-06-17 09:18:33 +0800

    rewrite unet for return local feature

--- csm/nnutils/unet.py
+++ csm/nnutils/unet.py
@@ -1,119 +1,117 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from absl import app
-from absl import flags
-import os
-import os.path as osp
-import numpy as np
 import torch
-import torchvision
 import torch.nn as nn
-from torch.autograd import Variable
-import functools
 
-from . import net_blocks as nb
-import pdb
-        
-#------ UNet style generator ------#
-#----------------------------------#
+from torchsummary import summary
+from torchviz import make_dot
 
-# Defines the Unet generator.
-# |num_downs|: number of downsamplings in UNet. For example,
-# if |num_downs| == 7, image of size 128x128 will become of size 1x1
-# at the bottleneck
-# concats additional features at the bottleneck
 class UnetConcatGenerator(nn.Module):
-    def __init__(self, input_nc, output_nc, num_downs, ngf=64,
-                 norm_layer=nn.modules.normalization.GroupNorm):
+    def __init__(self, input_nc, output_nc, num_downs, ngf=64, norm_layer = nn.modules.normalization.GroupNorm):
         super(UnetConcatGenerator, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
+        self.conv4 = nn.Conv2d(256, 512, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
+        self.conv5 = nn.Conv2d(512, 512, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
 
-        if num_downs >= 5:
-            ngf_max = ngf*8
-        else:
-            ngf_max = ngf*pow(2, num_downs - 2)
+        self.upsample = nn.Upsample(scale_factor=2.0, mode="bilinear")
+        self.ac = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        self.reflectionPad = nn.ReflectionPad2d((1, 1, 1, 1))
 
-
-        # construct unet structure
-        all_blocks = []
-        self.inner_most_block = unet_block = UnetSkipConnectionConcatBlock(ngf_max, ngf_max, input_nc=None, submodule=None, norm_layer=norm_layer, innermost=True)
-        all_blocks.append(unet_block)
-
-
-        for i in range(num_downs - 5):
-            unet_block = UnetSkipConnectionConcatBlock(ngf_max, ngf_max, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
-            all_blocks.append(unet_block)
-
-        for i in range(min(3, num_downs - 2)):
-            unet_block = UnetSkipConnectionConcatBlock(ngf_max // pow(2,i+1), ngf_max // pow(2,i), input_nc=None, submodule=unet_block, norm_layer=norm_layer)
-            all_blocks.append(unet_block)
-
-        unet_block = UnetSkipConnectionConcatBlock(output_nc, ngf, input_nc=input_nc, submodule=unet_block, outermost=True, norm_layer=norm_layer)
-        all_blocks.append(unet_block)
-
-        self.model = unet_block
-        self.all_blocks = all_blocks
-        nb.net_init(self.model)
-
-    def forward(self, input):
-        return self.model(input)
-
-    def get_inner_most(self, ):
-        return self.inner_most_block
-
-    def get_all_block(self, ):
-        return all_blocks
-
-
-# Defines the submodule with skip connection.
-# X -------------------identity---------------------- X
-#   |-- downsampling -- |submodule| -- upsampling --|
-class UnetSkipConnectionConcatBlock(nn.Module):
-    def __init__(self, outer_nc, inner_nc, input_nc=None,
-                 submodule=None, outermost=False, innermost=False, norm_layer=nn.modules.normalization.GroupNorm):
-        super(UnetSkipConnectionConcatBlock, self).__init__()
-        self.outermost = outermost
-        self.innermost = innermost
-
-        # if submodule is None:
-        #     pdb.set_trace()
-        if type(norm_layer) == functools.partial:
-            use_bias = norm_layer.func == nn.InstanceNorm2d
-        else:
-            use_bias = norm_layer == nn.InstanceNorm2d
-        if input_nc is None:
-            input_nc = outer_nc
-
-        downconv = nn.Conv2d(input_nc, inner_nc, kernel_size=4,
-                             stride=2, padding=1, bias=use_bias)
-        downrelu = nn.LeakyReLU(0.01, False)
-        uprelu = nn.ReLU(False)
-
-        if outermost:
-            self.down = [downconv]
-            self.up = [nb.upconv2d(inner_nc * 2, inner_nc), nn.Conv2d(inner_nc, outer_nc, kernel_size=3, stride=1, padding=1, bias=True)]
-        elif innermost:
-            self.down = [nb.conv2d(False, input_nc, inner_nc, kernel_size=4, stride=2)]
-            self.up = [nb.upconv2d(inner_nc, outer_nc)]
-        else:
-            self.down = [nb.conv2d(False, input_nc, inner_nc, kernel_size=4, stride=2)]
-            self.up = [nb.upconv2d(inner_nc * 2, outer_nc)]
-            
-        self.up = nn.Sequential(*self.up)
-        self.down = nn.Sequential(*self.down)
-        self.submodule = submodule
-
+        self.up_conv5 = nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1))
+        self.up_conv4 = nn.Conv2d(1024, 256, kernel_size=(3, 3), stride=(1, 1))
+        self.up_conv3 = nn.Conv2d(512, 128, kernel_size=(3, 3), stride=(1, 1))
+        self.up_conv2 = nn.Conv2d(256, 64, kernel_size=(3, 3), stride=(1, 1))
+        self.up_conv1 = nn.Conv2d(128, 64, kernel_size=(3, 3), stride=(1, 1))
+        self.up_conv1_1 = nn.Conv2d(64, 4, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        UV_HEIGHT, UV_WIDTH = 256, 256
+        self.resize = nn.UpsamplingBilinear2d(size = (UV_HEIGHT, UV_WIDTH))
 
     def forward(self, x):
         x_inp = x
-        self.x_enc = self.down(x_inp)
-        if self.submodule is not None:
-            out = self.submodule(self.x_enc)
-        else:
-            out = self.x_enc
-        self.x_dec = self.up(out)
-        if self.outermost:
-            return self.x_dec
-        else:
-            return torch.cat([x_inp, self.x_dec], 1)
+        d1 = self.conv1(x_inp)  # 256x256 --> 128x128
+
+        d2 = self.conv2(d1)  # 128x128 --> 64x64
+        d2 = self.ac(d2)
+
+        d3 = self.conv3(d2)  # 64x64 --> 32x32
+        d3 = self.ac(d3)
+
+        d4 = self.conv4(d3)  # 32x32 --> 16x16
+        d4 = self.ac(d4)
+
+        d5 = self.conv5(d4)  # 16x16 --> 8x8
+        d5 = self.ac(d5)
+
+        u5 = self.upsample(d5)
+        u5 = self.reflectionPad(u5)
+        u5 = self.up_conv5(u5)
+        u5 = self.ac(u5)
+
+        print(u5.shape)
+        print(d5.shape)
+        u5 = torch.cat([d4, u5], 1)
+
+        u4 = self.upsample(u5)
+        u4 = self.reflectionPad(u4)
+        u4 = self.up_conv4(u4)
+        u4 = self.ac(u4)
+        u4 = torch.cat([d3, u4], 1)
+
+        u3 = self.upsample(u4)
+        u3 = self.reflectionPad(u3)
+        u3 = self.up_conv3(u3)
+        u3 = self.ac(u3)
+        u3 = torch.cat([d2, u3], 1)
+
+        u2 = self.upsample(u3)
+        u2 = self.reflectionPad(u2)
+        u2 = self.up_conv2(u2)
+        u2 = self.ac(u2)
+        u2 = torch.cat([d1, u2], 1)
+
+        u1 = self.upsample(u2)
+        u1 = self.reflectionPad(u1)
+        u1 = self.up_conv1(u1)
+        u1 = self.ac(u1)
+
+        u1 = self.up_conv1_1(u1)
+
+        ans = [u1, u2, u3, u4]
+        resize_ans = []
+
+        for i in range(1, len(ans)):
+            u = ans[i]
+            resize_u = self.resize(u)
+            resize_ans.append(resize_u)
+        local_feature = torch.cat(resize_ans, 1)
+        return resize_ans,local_feature, u1
+
+
+# unet = UnetConcatGenerator()
+# BS = 32
+# IMG_CHANNEL = 3
+# IMG_WIDTH = 256
+# IMG_HEIGHT = 256
+# summary(unet, (IMG_CHANNEL, IMG_HEIGHT, IMG_WIDTH))
+
+
+
+# unet_gen = UnetConcatGenerator(input_nc=3, output_nc=(4), num_downs=5, )
+# BS = 32
+# IMG_CHANNEL = 3
+# IMG_WIDTH = 256
+# IMG_HEIGHT = 256
+# imgs = torch.rand((BS, IMG_CHANNEL, IMG_HEIGHT, IMG_WIDTH))
+# imgs2 = torch.rand((BS, IMG_CHANNEL, IMG_HEIGHT, IMG_WIDTH))
+# # ans = torch.cat([imgs, imgs2], 1)
+# print("1")
+# #
+# #y = unet_gen.forward(imgs)
+# ans = make_dot(unet_gen(imgs), params=dict(unet_gen.named_parameters()))
+# print(ans)
+
+
+
+
+
+
